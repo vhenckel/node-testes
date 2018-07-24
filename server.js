03170aff9d3f3163f00117c7c4c3f1deb79f9f14
Vitor Henckel <vitor@henckel.com.br>
2018-07-24 20:21:01 -0300

    Iniciando o debug

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route)
 server.listen(port)
 server.on('error', onError)
+server.on('listening', onListening)
 
 console.log("Servidor rodando na porta: " + port);
 
@@ -57,4 +58,12 @@ function onError (error) {
       default:
         throw error
     }
+}
+
+function onListening() {
+  const addr = server.address()
+  const bind = typeof addr == 'string'
+    ? 'pipe ' + addr 
+    : 'port ' + addr.port 
+  debug('Listening on ' + bind)
 }
